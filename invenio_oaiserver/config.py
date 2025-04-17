bf6bfaf5977a6957a8716cca6836d8e28a5f833b
Ronald Krist <ronald.krist@cesnet.cz>
2025-04-17 12:21:16 +0200

    configurable list sets

--- invenio_oaiserver/config.py
+++ invenio_oaiserver/config.py
@@ -132,6 +132,9 @@ OAISERVER_CREATED_KEY = "_created"
 OAISERVER_RECORD_SETS_FETCHER = "invenio_oaiserver.percolator:find_sets_for_record"
 """Record's OAI sets function."""
 
+OAISERVER_RECORD_LIST_SETS_FETCHER = "invenio_oaiserver.percolator:sets_search_all"
+"""Record's list OAI sets function."""
+
 OAISERVER_PERCOLATOR_DEDICATED_INDEX = True
 """Create a dedicated index for the percolators, instead of storing them in
 the same index as the records.
