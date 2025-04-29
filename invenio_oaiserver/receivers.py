388510583232858ee21c5e94da952e0b7f38d197
Ronald Krist <ronald.krist@cesnet.cz>
2025-04-29 13:09:50 +0200

    configurable new and delete percolator functions

--- invenio_oaiserver/receivers.py
+++ invenio_oaiserver/receivers.py
@@ -10,19 +10,20 @@
 """Record field function."""
 
 from .percolator import _delete_percolator, _new_percolator
+from .proxies import current_oaiserver
 
 
 def after_insert_oai_set(mapper, connection, target):
     """Update records on OAISet insertion."""
-    _new_percolator(spec=target.spec, search_pattern=target.search_pattern)
+    current_oaiserver.new_percolator_function(spec=target.spec, search_pattern=target.search_pattern)
 
 
 def after_update_oai_set(mapper, connection, target):
     """Update records on OAISet update."""
-    _delete_percolator(spec=target.spec, search_pattern=target.search_pattern)
-    _new_percolator(spec=target.spec, search_pattern=target.search_pattern)
+    current_oaiserver.delete_percolator_function(spec=target.spec, search_pattern=target.search_pattern)
+    current_oaiserver.new_percolator_function(spec=target.spec, search_pattern=target.search_pattern)
 
 
 def after_delete_oai_set(mapper, connection, target):
     """Update records on OAISet deletion."""
-    _delete_percolator(spec=target.spec, search_pattern=target.search_pattern)
+    current_oaiserver.delete_percolator_function(spec=target.spec, search_pattern=target.search_pattern)
